f2d9ba9814d6fe1fa9326bdbc81bb5f488207953
AssahBismarkabah <assah9799@gmail.com>
2024-02-08 22:47:16 +0100

    feature: added controller class

--- Game.py
+++ Game.py
@@ -37,22 +37,77 @@ class Game():
             self.cardXPositionsList.append(thisLeft)
             thisLeft = thisLeft + Game.CARD_OFFSET
 
+
         self.reset()  # start a round of the game
 
     def reset(self):  # this method is called when a new round starts
+        self.cardShuffleSound.play()
+        self.cardList = []
+        self.oDeck.shuffle()
+        for cardIndex in range(0, Game.NCARDS):  # deal out cards
+            oCard = self.oDeck.getCard()
+            self.cardList.append(oCard)
+            thisXPosition = self.cardXPositionsList[cardIndex]
+            oCard.setLoc((thisXPosition, Game.CARDS_TOP))
 
+
+        self.showCard(0)
+        self.cardNumber = 0
+        self.currentCardName, self.currentCardValue = \
+                                         self.getCardNameAndValue(self.cardNumber)
+
+        self.messageText.setValue('Starting card is ' + self.currentCardName +
+                                                '. Will the next card be higher or lower?')
 
     def getCardNameAndValue(self, index):
         oCard = self.cardList[index]
         theName = oCard.getName()
         theValue = oCard.getValue()
         return theName, theValue
+    
 
     def showCard(self, index):
         oCard = self.cardList[index]
         oCard.reveal()
 
-    def hitHigherOrLower(self, higherOrLower):
 
+    def hitHigherOrLower(self, higherOrLower):
+        self.cardNumber = self.cardNumber + 1
+        self.showCard(self.cardNumber)
+        nextCardName, nextCardValue = self.getCardNameAndValue(self.cardNumber)
+        
+
+        if higherOrLower == HIGHER:
+            if nextCardValue > self.currentCardValue:
+                self.score = self.score + Game.POINTS_CORRECT
+                self.messageText.setValue('Yes, the ' + nextCardName + ' was higher')
+                self.winnerSound.play()
+            else:
+                self.score = self.score - Game.POINTS_INCORRECT
+                self.messageText.setValue('No, the ' + nextCardName + ' was not higher')
+                self.loserSound.play()
+
+        else:  # user hit the Lower button
+            if nextCardValue < self.currentCardValue:
+                self.score = self.score + Game.POINTS_CORRECT
+                self.messageText.setValue('Yes, the ' + nextCardName + ' was lower')
+                self.winnerSound.play()
+            else:
+                self.score = self.score - Game.POINTS_INCORRECT
+                self.messageText.setValue('No, the ' + nextCardName + ' was not lower')
+                self.loserSound.play()
+
+        self.scoreText.setValue('Score: ' + str(self.score))
+
+        self.currentCardValue = nextCardValue  # set up for the next card 
+
+        done = (self.cardNumber == (Game.NCARDS - 1))  # did we reach the last card?
+        return done
 
     def draw(self):
+        # Tell each card to draw itself
+        for oCard in self.cardList:
+            oCard.draw()
+
+        self.scoreText.draw()
+        self.messageText.draw()
