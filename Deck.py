86e271503ff427e6a1e51e8aa320394fbdf4a5ab
T.Arthur <tcheutchouaarthur38@gmail.com>
2024-02-09 08:24:29 +0100

    deck

--- Deck.py
+++ Deck.py
@@ -1,32 +1,30 @@
-# Deck class
-
 import random
 from Card import *
 
-class Deck():
-    #class variable containing the four suits
-
+class Deck:
     SUIT_TUPLE = ('Diamonds', 'Clubs', 'Hearts', 'Spades')
-
-    # This dict maps each card rank to a value for a standard deck of cards
-
-    STANDARD_DICT = {'Ace':1, '2':2, '3':3, '4':4, '5':5,
-                                  '6':6, '7':7, '8': 8, '9':9, '10':10,
-                                  'Jack':11, 'Queen':12, 'King':13}
-
+    STANDARD_DICT = {'Ace': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+                     '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+                     'Jack': 11, 'Queen': 12, 'King': 13}
 
     def __init__(self, window, rankValueDict=STANDARD_DICT):
+        self.window = window
+        self.rankValueDict = rankValueDict
+        self.decklist = []
 
-    #create the cards and save in an array using a loop to
-    # iterate through the suit, rank and value and append each
-    #card object in a list to create a full deck of cards
+        # Create the cards and save them in an array by iterating through the suit, rank, and value
+        for suit in self.SUIT_TUPLE:
+            for rank, value in self.rankValueDict.items():
+                card = Card(self.window, suit, rank, value)
+                self.decklist.append(card)
 
     def shuffle(self):
-    # Copy the starting deck and save it in the playing decklist but before that all
-    #cards should be concealed(faced down)
+        # Shuffle the decklist using the random.shuffle() function
+        random.shuffle(self.decklist)
 
     def getCard(self):
-
-    # Pop one card off the deck and return it since the deck is already shuffled
-    #orderwise through an error message
-
+        if len(self.decklist) > 0:
+            # Pop one card off the decklist and return it
+            return self.decklist.pop()
+        else:
+            print("Error: No cards left in the deck.")
